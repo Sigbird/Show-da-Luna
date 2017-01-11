78f376ed4b5860fde404a98c16afe14748939d67
Ivan Quirino <ivan@yupistudios.com>
2017-01-11 18:39:56 -0300

    Build OK:
    Compras OK
    Vídeos OK
    Problemas gráficos
    Pintura no Facebook traba o app

--- Assets/Scripts/CSharp/YupiStudios/Luna/PlayGamesServices/GamesServicesDialog.cs
+++ Assets/Scripts/CSharp/YupiStudios/Luna/PlayGamesServices/GamesServicesDialog.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using GooglePlayGames;
 
 public class GamesServicesDialog : MonoBehaviour {
 
 	public GameObject Container;
 
 	void Start() {
-		if (!Social.localUser.authenticated) {
-			Container.SetActive(true);
-		}
+
+//		if (!PlayerPrefs.HasKey(GameSave.LOADEDSAVEKEY)) {
+//			Container.SetActive(true);
+//		} else {
+//			GamesServicesSignIn.SignIn();
+//		}
 	}
 	public void CallSignIn() {
 		GamesServicesSignIn.SignIn();
---
+++ Assets/Scripts/CSharp/YupiStudios/Luna/PlayGamesServices/GamesServicesDialogButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamesServicesDialogButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (PlayerPrefs.HasKey(GameSave.LOADEDSAVEKEY)) {
+			GamesServicesSignIn.SignIn();
+			this.gameObject.SetActive(false);
+		} 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
