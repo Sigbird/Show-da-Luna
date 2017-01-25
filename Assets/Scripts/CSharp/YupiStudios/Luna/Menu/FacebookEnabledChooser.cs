687bf7a054b920e07856bab110048c63d4c65d17
Ivan Quirino <ivan@yupistudios.com>
2017-01-25 20:38:48 -0300

    habilita/desabilita Facebook no quintal

--- Assets/Scripts/CSharp/YupiStudios/Luna/Menu/FacebookEnabledChooser.cs
+++ Assets/Scripts/CSharp/YupiStudios/Luna/Menu/FacebookEnabledChooser.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FacebookEnabledChooser : MonoBehaviour {
-	public GameObject FacebookEnabled;
-	public GameObject FacebookDisabled;
+	public GameObject[] FacebookEnabled;
+	public GameObject[] FacebookDisabled;
 	// Use this for initialization
 	void Start () {
-		if (!YupiPlay.Luna.BuildConfiguration.FacebookEnabled) {
-			FacebookEnabled.SetActive(false);
-			FacebookDisabled.SetActive(true);
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		if (YupiPlay.Luna.BuildConfiguration.FacebookEnabled) {
+			foreach (GameObject item in FacebookEnabled) {
+				item.SetActive(true);	
+			}
+			foreach (GameObject item in FacebookDisabled) {
+				item.SetActive(false);	
+			}				
+		} else {
+			foreach (GameObject item in FacebookEnabled) {
+				item.SetActive(false);	
+			}
+			foreach (GameObject item in FacebookDisabled) {
+				item.SetActive(true);	
+			}
+		}			
 	}
 }
