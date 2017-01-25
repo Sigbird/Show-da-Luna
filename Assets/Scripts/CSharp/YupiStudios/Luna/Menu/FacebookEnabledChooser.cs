c067508d3d9e9e86368c72ac6c6f44b70bd7b71b
Ivan Quirino <ivan@yupistudios.com>
2017-01-24 22:36:29 -0300

    desabilita facebook no GameEnd.unity se estiver setado na configuração

---
+++ Assets/Scripts/CSharp/YupiStudios/Luna/Menu/FacebookEnabledChooser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacebookEnabledChooser : MonoBehaviour {
+	public GameObject FacebookEnabled;
+	public GameObject FacebookDisabled;
+	// Use this for initialization
+	void Start () {
+		if (!YupiPlay.Luna.BuildConfiguration.FacebookEnabled) {
+			FacebookEnabled.SetActive(false);
+			FacebookDisabled.SetActive(true);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
