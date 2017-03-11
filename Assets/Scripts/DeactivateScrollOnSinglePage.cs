5720c93accacf56043c6cda1e581ee7edf76f257
Ivan Quirino <ivan@yupistudios.com>
2017-03-11 13:58:24 -0300

    Tratamento das coleções e paginação

---
+++ Assets/Scripts/DeactivateScrollOnSinglePage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeactivateScrollOnSinglePage : MonoBehaviour {
+	public GameObject Container;
+	// Use this for initialization
+	void Start () {
+		if (Container.transform.childCount == 1) {
+			ScrollRect scrollRect = GetComponent<ScrollRect>();
+			scrollRect.enabled = false;
+			ScrollSnapRect snap = GetComponent<ScrollSnapRect>();
+			snap.enabled = false;
+		}			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/LanguagePaginationHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YupiStudios.API.Language;
+
+public class LanguagePaginationHandler : MonoBehaviour {
+	public LanguageChooser.LanguageObject[] LanguageObjects;
+	// Use this for initialization
+	void Awake () {
+		SystemLanguage lang = Application.systemLanguage;
+
+		foreach (LanguageChooser.LanguageObject l in LanguageObjects) {
+			if (l.Language == lang) {
+				l.ObjectToActivate.transform.parent = null;
+				Destroy(l.ObjectToActivate);
+			}
+		}
+	}
+
+	void Start() {
+		
+	}
+
+}
