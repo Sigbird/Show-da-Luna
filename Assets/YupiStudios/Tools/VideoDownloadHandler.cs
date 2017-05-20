d028c950d96b08e83fd909908fcfc21cf85ba9aa
Ivan Quirino <ivan@yupistudios.com>
2017-05-20 12:42:03 -0300

    VideoDownloadHandler.cs

---
+++ Assets/YupiStudios/Tools/VideoDownloadHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.IO;
+using System;
+
+
+public class VideoDownloadHandler : DownloadHandlerScript {
+    private string fileName;
+
+    public VideoDownloadHandler(string fileName) : base() {
+        this.fileName = fileName;
+    }
+
+    protected void OnContentComplete() {
+        byte[] videoData = this.data;
+
+        if (isDone) {
+            try {
+                FileStream stream = File.OpenWrite(fileName);
+
+                stream.Write(videoData, 0, data.Length);
+                stream.Close();
+                // return true;
+            } catch (Exception e) {
+                Debug.LogError(e.Message);
+                // return false;
+            }
+        }
+        
+    }
+    
+}
