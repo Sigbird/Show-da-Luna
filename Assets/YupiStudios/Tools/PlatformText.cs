563f3f048f9223bb41cd60742e55db7bffa38ed2
Ivan Quirino <ivan@yupistudios.com>
2018-02-13 21:47:02 -0300

    PlatformText
    falta ajustar o PlatformText do GameServicesDialog na cena do jogo principal.

---
+++ Assets/YupiStudios/Tools/PlatformText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlatformText : MonoBehaviour {
+	[TextArea]
+	public string IOSText;
+
+	private Text textComponent;
+
+	void Start () {
+		textComponent = GetComponent<Text> ();
+
+		#if UNITY_IOS 
+		setText(IOSText);
+		#endif
+	}
+
+	private void setText(string text) {
+		if (!string.IsNullOrEmpty(text)) {
+			textComponent.text = text;
+		}			
+	}
+}
